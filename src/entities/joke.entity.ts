26770d90068a5f06c04d8ac1251eca2345b73171
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2022-03-25 10:16:45 -0300

    Create entity with decorators

---
+++ src/entities/joke.entity.ts
@@ -0,0 +1,24 @@
+import { Field, Int } from '@nestjs/graphql';
+
+export class Joke {
+  @Field((type) => Int)
+  id: string;
+
+  @Field((type) => String)
+  created_at: string;
+
+  @Field((type) => String)
+  updated_at: string;
+
+  @Field((type) => [String])
+  categories: string[];
+
+  @Field((type) => String)
+  icon_url: string;
+
+  @Field((type) => String)
+  url: string;
+
+  @Field((type) => String)
+  value: string;
+}
