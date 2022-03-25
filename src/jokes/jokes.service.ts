f8eaaa98c3f110d4aa7c93e0987ba8a4fa9a81a7
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2022-03-25 11:14:19 -0300

    Service with randomJoke

--- src/jokes/jokes.service.ts
+++ src/jokes/jokes.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { Joke } from 'src/entities/joke.entity';
 
 @Injectable()
-export class JokesService {}
+export class JokesService {
+  api = axios.create({
+    baseURL: 'https://api.chucknorris.io/jokes/',
+  });
+
+  async getRandomJoke(): Promise<Joke> {
+    const { data } = await this.api.get('random');
+    return new Joke({ ...data });
+  }
+}
