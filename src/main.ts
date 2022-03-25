01c719549d315d2ddebc0bfae43f5ea8e7f51784
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2022-03-25 13:38:24 -0300

    Added port with or without environment variable

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
