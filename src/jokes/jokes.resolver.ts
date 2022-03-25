920553d7b8e82ed9b7021a5598f11269e153a235
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2022-03-25 11:14:08 -0300

    Resolver with randomJoke

--- src/jokes/jokes.resolver.ts
+++ src/jokes/jokes.resolver.ts
@@ -1,4 +1,13 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query } from '@nestjs/graphql';
+import { Joke } from 'src/entities/joke.entity';
+import { JokesService } from './jokes.service';
 
-@Resolver()
-export class JokesResolver {}
+@Resolver((of) => Joke)
+export class JokesResolver {
+  constructor(private readonly jokesService: JokesService) {}
+
+  @Query((returns) => Joke)
+  randomJoke(): Promise<Joke> {
+    return this.jokesService.getRandomJoke();
+  }
+}
